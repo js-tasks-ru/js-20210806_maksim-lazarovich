d8a42d235d0882d2fb1d1fe6fb6f0ef31b99edb9
meedow20 <55607223+meedow20@users.noreply.github.com>
2021-08-11 18:18:32 +0300

    second task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,9 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  return (
+    param === 'asc' ?
+      [...arr].sort((a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'})) :
+      [...arr].sort((a, b) => b.localeCompare(a, ['ru', 'en'], {caseFirst: 'upper'}))
+  );
 }
