b5d5f3ecf3e8c0bf73aa24df643d1d7b680c3c85
meedow20 <55607223+meedow20@users.noreply.github.com>
2021-08-12 14:43:19 +0300

    fix: fixed sortStrings function and small things

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,9 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  return (
-    param === 'asc' ?
-      [...arr].sort((a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'})) :
-      [...arr].sort((a, b) => b.localeCompare(a, ['ru', 'en'], {caseFirst: 'upper'}))
-  );
+  const locales = ['ru', 'en'];
+  const options = {caseFirst: 'upper'};
+
+  const compareStrings = (a, b) => param === 'asc' ? a.localeCompare(b, locales, options) : b.localeCompare(a, locales, options);
+
+  return [...arr].sort(compareStrings);
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,7 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-  let omitObj = {};
+  const omitObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
     if (!fields.includes(key)) {
